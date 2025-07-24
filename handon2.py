0288d4f686aa18e900909f80caa73e573c39c9df
Godwinhos <godwinbenjamin2x18.com>
2025-07-24 16:25:44 +0100

    second tasks

---
+++ handon2.py
@@ -0,0 +1,18 @@
+genres = ["Adventure", "Comedy", "Animation", "Fantasy", "Sci-Fi", "Documentary", "Fantasy"]
+print(genres)
+
+#to add drama
+genres.append("drama")
+print(genres)
+
+#to remove fantasy
+genres.remove(genres[3])
+print(genres)
+
+#organisers want to know list of genres
+genres = len(genres)
+print(genres)
+
+
+genres = genres[1]
+print(meals) 
